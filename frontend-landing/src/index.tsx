90dffb6d142619c6a06023325459866e9a341bd0
gsantini-dogegf <gsantini@dogegf.com>
2022-01-17 11:25:54 -0500

    add pop up

--- frontend-landing/src/index.tsx
+++ frontend-landing/src/index.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/styles/main.scss";
 import App from "./app/App";
-import { IntercomProvider } from "react-use-intercom";
-
-const INTERCOM_APP_ID = "jcabc7e3";
+import Popup from "app/components/Dialog/Popup";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <IntercomProvider children autoBoot appId={INTERCOM_APP_ID} />
+    <Popup />
   </React.StrictMode>,
   document.getElementById("root")
 );

502975bb94ce0df423fcd1316bb54a0c95218f75
gsantini-dogegf <gsantini@dogegf.com>
2022-01-17 07:04:10 -0500

    add intercom

--- frontend-landing/src/index.tsx
+++ frontend-landing/src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/styles/main.scss';
-import App from './app/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/styles/main.scss";
+import App from "./app/App";
+import { IntercomProvider } from "react-use-intercom";
+
+const INTERCOM_APP_ID = "jcabc7e3";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <IntercomProvider children autoBoot appId={INTERCOM_APP_ID} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
